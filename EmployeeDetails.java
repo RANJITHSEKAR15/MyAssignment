45ed679e7ec13ab6fde90b52ef1d893a6fb67063
RANJITHSEKAR15 <ranjithsekar123@gmail.com>
2025-08-16 23:09:31 -0500

    Add files via upload

---
+++ EmployeeDetails.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		         String name = "John Doe";
+				 byte age = 30;
+				 float salary = 55000.75f;
+				 char gender = 'M';
+				 boolean acceptTerms = true;
+				 float rating = 4.5f;
+			     long mobileNumber = 9876543210L;
+				int pinCode = 56001;
+				byte luckyNumber = 7;
+				
+				System.out.println("Name of the person: "+name+"\n"+"Age of the person: "+age+"\n"+"Salary of the person: "+salary+"\n"+"Gender of the person: "+gender+"\n"+
+				"Accepted T&C: "+acceptTerms+"\n"+"Rating of the person: "+rating+"\n"+"Mobile number: "+mobileNumber+"\n"+"Pincode: "+pinCode+"\n"+
+						"Lucky Number: "+luckyNumber);
+
+	}
+
+}
